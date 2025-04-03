e1ced856558c4ad0dcb712205308013b03b61d49
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2025-04-03 15:48:07 -0300

    new

--- js/script-toggle.js
+++ js/script-toggle.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //toggle entre login e cadastro
-    $('#loginToggle').click(function(){
+    $('#loginToggle').click(function () {
         $(this).addClass('active');
         $('#cadastroToggle').removeClass('active');
         $('#loginForm').addClass('active');
@@ -8,12 +8,29 @@ $(document).ready(function(){
     });
 
 
-    $('#cadastroToggle').click(function(){
+    $('#cadastroToggle').click(function () {
         $(this).addClass('active');
         $('#loginToggle').removeClass('active');
         $('#cadastroForm').addClass('active');
         $('#loginForm').removeClass('active');
     });
 
-    
+    // Toggle para mostrar/esconder senha
+    function setupPasswordToggle(toggleId, inputId) {
+        $(toggleId).click(function () {
+            const input = $(inputId);
+            if (input.attr('type') === 'password') {
+                input.attr('type', 'text');
+                $(this).removeClass('fa-eye').addClass('fa-eye-slash');
+            } else {
+                input.attr('type', 'password');
+                $(this).removeClass('fa-eye-slash').addClass('fa-eye');
+            }
+        });
+    }
+
+    setupPasswordToggle('#toggleLoginPassword', '#loginPassword');
+    setupPasswordToggle('#toggleCadastroPassword', '#CadastroPassword');
+    setupPasswordToggle('#toggleCadastroConfirmPassword', '#cadastroConfirmPassword');
+
 })
